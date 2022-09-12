7161d95cce0ec281ae918c96e2bdb76f2e210a96
danjane <jdtjane@gmail.com>
2022-09-12 15:08:26 +0200

    test is member

--- tests/scrapeClassLists_test.py
+++ tests/scrapeClassLists_test.py
@@ -3,3 +3,6 @@ import scrapeClassLists
 
 def test_ScrapeClassList():
     assert scrapeClassLists.student_id("dan.jn,,,,,,,,,,,") == "dan.jn"
+
+def test_membre():
+    assert scrapeClassLists.is_member("dan.jn,,membre,,,,,,,,,")
