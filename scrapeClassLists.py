8460471709d3a77dc881c8da495a7a9b0df5bc99
danjane <jdtjane@gmail.com>
2022-09-12 15:11:19 +0200

    test is member passes

--- scrapeClassLists.py
+++ scrapeClassLists.py
@@ -4,3 +4,8 @@ import re
 def student_id(str):
     matches = re.findall("^[a-z]+\.[a-z1-9]*", str)
     return matches[0]
+
+
+def is_member(str):
+    matches = re.findall("(?!,)membre(?=,)", str)
+    return len(matches) == 1
