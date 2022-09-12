75a80abc24739c54c669b3ba1e7a2e0932109e58
danjane <jdtjane@gmail.com>
2022-09-12 15:26:20 +0200

    read a list of strings and pull out student ids, realistic test

--- tests/scrapeClassLists_test.py
+++ tests/scrapeClassLists_test.py
@@ -16,3 +16,15 @@ def test_ScrapeLinesSimple():
         "dan.jn,,prop,,,,"
     ])
     assert ids == ["dan.jn", "dan.jn"]
+
+
+def test_ScrapeLinesFull():
+    ids = """
+Membres du groupe rousseau-classe-106
+Adresse e-mail,Pseudo,État du groupe,État de l'adresse e-mail,Préférences relatives aux e-mails,Droits de publication,Année d'abonnement,Mois d'abonnement,Jour d'abonnement,Heure d'abonnement,Minute d'abonnement,Seconde d'abonnement,Fuseau horaire
+dan.jn@eduge.ch,,membre,,e-mails,autorisé,2022,8,14,20,0,13,heure d’été d’Europe centrale
+dan.jn2@eduge.ch,,propriétaire,,e-mails,autorisé,2022,8,14,20,0,15,heure d’été d’Europe centrale
+dan.jn3@eduge.ch,,membre,,e-mails,autorisé,2022,8,14,20,0,20,heure d’été d’Europe centrale
+dan.jn4@eduge.ch,,membre,,e-mails,autorisé,2022,8,14,20,0,21,heure d’été d’Europe centrale
+""".split("\n")
+    assert scrapeClassLists.student_ids(ids) == ["dan.jn", "dan.jn3", "dan.jn4"]
