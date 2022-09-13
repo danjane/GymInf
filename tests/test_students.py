649c5cb708333ca6ff9f45cebb5b1c8ffcf12c2f
danjane <jdtjane@gmail.com>
2022-09-13 21:57:08 +0200

    extra test of code name

--- tests/test_students.py
+++ tests/test_students.py
@@ -14,9 +14,11 @@ def test_givenName():
 def test_codeToName():
     c, n = code_and_name("dan.jn")
     assert n == "Dan"
+    assert c == "dan.jn"
 
 
 def test_codeToGivenName():
     c, n = code_and_name("dan.jn, Danny")
     assert n == "Danny"
+    assert c == "dan.jn"
 
