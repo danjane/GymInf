8f3c0c0fd2ac7f387ba15d7e312c391ff98516fd
danjane <jdtjane@gmail.com>
2022-09-13 22:06:32 +0200

    minor

--- students.py
+++ students.py
@@ -5,7 +5,7 @@ def code_and_name(s_dirty):
     s = s_dirty.strip().split(', ')
     code = s[0]
     if len(s) == 1:
-        name = re.search(r'^[a-z\-]+', code).group(0).capitalize()
+        name = re.search(r"^[a-z\-]+", code).group(0).capitalize()
     else:
         name = s[1]
     return code, name
