2e309523ea40ae0ec90fcced82b0d24630aa012c
danjane <jdtjane@gmail.com>
2022-09-13 21:05:57 +0200

    failing test when passing a student's given name

--- tests/test_students.py
+++ tests/test_students.py
@@ -1,6 +1,11 @@
 import students
 
+c = students.Students()
 
-def test_students():
-    c = students.Students()
+
+def test_oneStudent():
     assert c['albert.enstn'] == "Albert"
+
+
+def test_givenName():
+    assert c['gabriel.crmr'] == "Gabs"
