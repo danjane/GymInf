8cf3b1ff4802fe6de853bc624e90a694a84d1aac
danjane <jdtjane@gmail.com>
2022-09-17 10:29:00 +0200

    We have one default path to the root of the folder structure

--- tests/config_test.py
+++ tests/config_test.py
@@ -5,3 +5,11 @@ c = config.load("../example_files/config.yaml")
 
 def test_loadClass():
     assert c["courses"] == ["1ma1dfb01"]
+
+
+def test_coursePath():
+    assert c["courses_path"] == "../example_files/"
+
+
+def test_configPath():
+    assert c["config_path"] == "../example_files/"
