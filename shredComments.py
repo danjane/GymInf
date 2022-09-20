b15d63604542993b7fdef281c3f24802ab3342a2
danjane <jdtjane@gmail.com>
2022-09-20 22:50:09 +0200

    refactor

--- shredComments.py
+++ shredComments.py
@@ -10,12 +10,12 @@ def find_students(string, students):
     return [student for student in students if is_student(string, student)]
 
 
-def update(current_info, line):
+def update(current_info, line, students):
     char, info = line[0], line[1:]
     if char == 'd':
         current_info["Date"] = datetime.datetime.strptime(info, "%d%b%Y").date()
     elif char == 'c':
         current_info["Course"] = info
     elif char == "+":
-        current_info["Students"] = ["Albert"]
+        current_info["Students"] = find_students(info, students)
     return current_info
--- tests/shredThoughts_test.py
+++ tests/shredThoughts_test.py
@@ -25,23 +25,23 @@ def test_findSubsetStudents():
 def test_newDate():
     # current_info a dictionary with Date, Course, Students and Comment
     current_info = {}
-    current_info = update(current_info, "d13Jan1982")
+    current_info = update(current_info, "d13Jan1982", ["Albert", "Gabs"])
     assert current_info["Date"] == datetime.date(1982, 1, 13)
 
 
 def test_newCourse():
     current_info = {}
-    current_info = update(current_info, "c1ma1df01")
+    current_info = update(current_info, "c1ma1df01", ["Albert", "Gabs"])
     assert current_info["Course"] == "1ma1df01"
 
 
 def test_newInfoForOneStudent():
     current_info = {}
-    current_info = update(current_info, "+Albert good question")
+    current_info = update(current_info, "+Albert good question", ["Albert", "Gabs"])
     assert current_info["Students"] == ["Albert"]
 
 
 def test_newInfoForTwoStudent():
     current_info = {}
-    current_info = update(current_info, "+Albert, Gabs good question")
+    current_info = update(current_info, "+Albert, Gabs good question", ["Albert", "Gabs"])
     assert current_info["Students"] == ["Albert", "Gabs"]
