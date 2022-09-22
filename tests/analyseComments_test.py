ea4b21e9691d9aee19b4213c4ae90c9f8c696bee
danjane <jdtjane@gmail.com>
2022-09-22 10:06:25 +0200

    test failing

--- tests/analyseComments_test.py
+++ tests/analyseComments_test.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 
 def test_rv():
+    d = {'Student': ["Albert"]*2, 'DNF': [1, 0]}
+    df = pd.DataFrame(data=d)
+    assert dnf_count(df)["Albert"] == 1
+
+
+def test_rv2():
     d = {'Student': ["Albert"]*2, 'DNF': [1, 1]}
     df = pd.DataFrame(data=d)
     assert dnf_count(df)["Albert"] == 2
