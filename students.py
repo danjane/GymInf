187e653563484b80141c10ddaf48a02a07c021b1
danjane <jdtjane@gmail.com>
2022-10-07 17:35:51 +0200

    test passing: Students object should just be a simple dictionary. test failing: first names

--- students.py
+++ students.py
@@ -23,32 +23,19 @@ def name_conflict(name_dict):
     return duplicates
 
 
-class Students:
-
-    def __init__(self, filename):
-        with open(filename, 'r') as f:
-            values = {}
-            for s_dirty in f.readlines():
-                c, n = code_and_name(s_dirty)
-                values[c] = n
-        self.values = values
-
-    @property
-    def values(self):
-        return self._values
-
-    @values.setter
-    def values(self, values):
-        self._values = values
-
-    def __getitem__(self, key):
-        return self._values[key]
+def course_list(filename):
+    with open(filename, 'r') as f:
+        values = {}
+        for s_dirty in f.readlines():
+            c, n = code_and_name(s_dirty)
+            values[c] = n
+    return values
 
 
 def loadClassLists(course_names, class_paths):
     classes = {}
     for cls, path in zip(course_names, class_paths):
-        classes[cls] = Students(path)
+        classes[cls] = course_list(path)
     return classes
 
 
