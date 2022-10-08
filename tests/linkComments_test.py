3e95d9bbfc74f4de0eba1b334c465c7f19e8a4b3
danjane <jdtjane@gmail.com>
2022-10-08 15:05:54 +0200

    test failing for building a latex report driven from the config.yaml

--- tests/linkComments_test.py
+++ tests/linkComments_test.py
@@ -5,3 +5,7 @@ def test_reportDnfs():
     dnfs = report_dnfs("../example_files/config.yaml")
     assert type(dnfs) is dict
 
+
+def test_buildLatexReport():
+    report = latex_report("../example_files/config.yaml")
+    assert type(report) is str
