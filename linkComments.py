2a011d4fc6809f4d502826f72773572449d2916b
danjane <jdtjane@gmail.com>
2022-10-08 15:41:12 +0200

    test passing to build the latex report drive from the config file

--- linkComments.py
+++ linkComments.py
@@ -18,3 +18,25 @@ def report_dnfs(cfg_path):
     for student, num in dnfs.items():
         print(f"{student}: {num}")
     return dnfs
+
+
+def latex_report(cfg_path):
+    cfg = config.load(cfg_path)
+    courses = students.loadClassLists(
+        cfg["courses"],
+        cfg["class_paths"])
+
+    with open(cfg["comments_path"], "r") as f:
+        strings = f.readlines()
+    infos = shredComments.shredder(strings, courses)
+    df = shredComments.panderize(infos)
+
+    with open(cfg["report_student_path"]) as f:
+        student_report_outline = f.read()
+
+    course_name = cfg["courses"][0]
+
+    codes = courses[course_name].keys()
+    names = courses[course_name].values()
+    return analyseComments.latex_report(df, student_report_outline, codes, names, course_name)
+
--- tests/linkComments_test.py
+++ tests/linkComments_test.py
@@ -3,9 +3,13 @@ from linkComments import *
 
 def test_reportDnfs():
     dnfs = report_dnfs("../example_files/config.yaml")
-    assert type(dnfs) is dict
+    output = dnfs
+    print(output)
+    assert type(output) is dict
 
 
 def test_buildLatexReport():
     report = latex_report("../example_files/config.yaml")
-    assert type(report) is str
+    output = report
+    print(output)
+    assert type(output) is str
