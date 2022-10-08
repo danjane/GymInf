9ed978663eb4f45e388d0cc929aabf931b3a7c9a
danjane <jdtjane@gmail.com>
2022-10-08 15:49:05 +0200

    refactor

--- linkComments.py
+++ linkComments.py
@@ -4,16 +4,20 @@ import students
 import config
 
 
-def report_dnfs(cfg_path):
+def load_from_config(cfg_path):
     cfg = config.load(cfg_path)
     courses = students.loadClassLists(
         cfg["courses"],
         cfg["class_paths"])
-
     with open(cfg["comments_path"], "r") as f:
         strings = f.readlines()
     infos = shredComments.shredder(strings, courses)
     df = shredComments.panderize(infos)
+    return cfg, courses, df
+
+
+def report_dnfs(cfg_path):
+    cfg, courses, df = load_from_config(cfg_path)
     dnfs = analyseComments.dnf_count_greater_than(df, 0)
     for student, num in dnfs.items():
         print(f"{student}: {num}")
@@ -21,15 +25,7 @@ def report_dnfs(cfg_path):
 
 
 def latex_report(cfg_path):
-    cfg = config.load(cfg_path)
-    courses = students.loadClassLists(
-        cfg["courses"],
-        cfg["class_paths"])
-
-    with open(cfg["comments_path"], "r") as f:
-        strings = f.readlines()
-    infos = shredComments.shredder(strings, courses)
-    df = shredComments.panderize(infos)
+    cfg, courses, df = load_from_config(cfg_path)
 
     with open(cfg["report_student_path"]) as f:
         student_report_outline = f.read()
