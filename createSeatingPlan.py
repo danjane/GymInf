96a045a536d49b6a34c302d914ecc3730cd1bce8
danjane <jdtjane@gmail.com>
2022-10-23 20:26:34 +0200

    passing test for seating plan latex eight desks in standard six by four arrangement

--- createSeatingPlan.py
+++ createSeatingPlan.py
@@ -3,9 +3,17 @@ def latexDeskNode(name, x, y):
 
 
 def latexDeskPositionsSixByFourStandard(num_students):
-    return None
+    num_rows = (num_students - 1) // 6 + 1
+    xs = [0, 2, 5, 7, 10, 12] * num_rows
+    ys = []
+    for row in range(num_rows):
+        ys += [row*4]*6
+    xs = xs[:num_students]
+    ys = ys[:num_students]
+    return xs, ys
 
 
 def seatingListToLatexDesks(students):
-    latex_desks = [latexDeskNode(s, x, 0) for s, x in zip(students, [0, 2])]
+    xs, ys = latexDeskPositionsSixByFourStandard(len(students))
+    latex_desks = [latexDeskNode(s, x, y) for s, x, y in zip(students, xs, ys)]
     return '\n'.join(latex_desks)
