0309dd05f7f0c3939d2416d301f6702b59738439
danjane <jdtjane@gmail.com>
2022-10-23 20:17:27 +0200

    failing test for seating plan latex eight desks

--- createSeatingPlan.py
+++ createSeatingPlan.py
@@ -2,6 +2,10 @@ def latexDeskNode(name, x, y):
     return f"\\node[desk] at ({x}, {y}) {{{name}}};"
 
 
+def latexDeskPositionsSixByFourStandard(num_students):
+    return None
+
+
 def seatingListToLatexDesks(students):
-    latex_desks = [latexDeskNode(s, x, 0) for s, x in zip(students, [0, 2.5])]
+    latex_desks = [latexDeskNode(s, x, 0) for s, x in zip(students, [0, 2])]
     return '\n'.join(latex_desks)
--- tests/createSeatingPlan_test.py
+++ tests/createSeatingPlan_test.py
@@ -1,8 +1,21 @@
 from createSeatingPlan import *
 
 
-def test_seatingListToLatexDesks():
+def test_seatingListTwoStudentsToLatexDesks():
     students = ["albert", "gabs"]
     output = r"""\node[desk] at (0, 0) {albert};
-\node[desk] at (2.5, 0) {gabs};"""
+\node[desk] at (2, 0) {gabs};"""
+    assert seatingListToLatexDesks(students) == output
+
+
+def test_seatingListEightStudentsToLatexDesks():
+    students = list("abcdefgh")
+    output = r"""\node[desk] at (0, 0) {a};
+\node[desk] at (2, 0) {b};
+\node[desk] at (5, 0) {c};
+\node[desk] at (7, 0) {d};
+\node[desk] at (10, 0) {e};
+\node[desk] at (12, 0) {f};
+\node[desk] at (0, 4) {g};
+\node[desk] at (2, 4) {h};"""
     assert seatingListToLatexDesks(students) == output
