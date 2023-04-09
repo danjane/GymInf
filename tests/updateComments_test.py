586942813590285cd27db77c1a04027491703e4d
danjane <jdtane@gmail.com>
2023-04-09 19:28:31 +0200

    failing test for negative comments

--- tests/updateComments_test.py
+++ tests/updateComments_test.py
@@ -3,7 +3,7 @@ import pytest
 from updateComments import add_positive_comments
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def comment_file():
     comment_test_file = "../example_files/testing_comments.txt"
     if os.path.exists(comment_test_file):
@@ -22,3 +22,8 @@ def check_file_contents(test_file, expected_text):
 def test_add_positive_comment(comment_file):
     add_positive_comments(comment_file, ["Julian", "Dick"])
     assert check_file_contents(comment_file, "+Julian, Dick\n")
+
+
+def test_add_negative_comment(comment_file):
+    add_negative_comments(comment_file, ["Julian", "Dick"])
+    assert check_file_contents(comment_file, "-Julian, Dick\n")
