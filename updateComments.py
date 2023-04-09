889e5512840dfdd592babe046e7e1ba95b0d2692
danjane <jdtane@gmail.com>
2023-04-09 19:31:18 +0200

    refactor

--- updateComments.py
+++ updateComments.py
@@ -1,10 +1,12 @@
-def add_positive_comments(comments_filename, student_names):
+def add_comments(comments_filename, student_names, prefix):
     with open(comments_filename, "a") as f:
-        line = "+{}\n".format(", ".join(student_names))
+        line = "{}{}\n".format(prefix, ", ".join(student_names))
         f.write(line)
+
+
+def add_positive_comments(comments_filename, student_names):
+    add_comments(comments_filename, student_names, "+")
 
 
 def add_negative_comments(comments_filename, student_names):
-    with open(comments_filename, "a") as f:
-        line = "-{}\n".format(", ".join(student_names))
-        f.write(line)
+    add_comments(comments_filename, student_names, "-")
