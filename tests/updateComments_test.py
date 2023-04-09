3de7c4c8faf454b693147506f9d3f5c505fb1327
danjane <jdtane@gmail.com>
2023-04-09 19:25:49 +0200

    refactor to use pytest fixture with automatic teardown after a yield

--- tests/updateComments_test.py
+++ tests/updateComments_test.py
@@ -1,19 +1,24 @@
+import os
+import pytest
 from updateComments import add_positive_comments
 
-comment_test_file = "../example_files/testing_comments.txt"
+
+@pytest.fixture
+def comment_file():
+    comment_test_file = "../example_files/testing_comments.txt"
+    if os.path.exists(comment_test_file):
+        os.remove(comment_test_file)
+
+    yield comment_test_file
+    os.remove(comment_test_file)
 
 
-def check_file_contents(expected_text):
-    with open(comment_test_file, 'r') as file:
+def check_file_contents(test_file, expected_text):
+    with open(test_file, 'r') as file:
         file_contents = file.read()
     return file_contents == expected_text
 
 
-def test_add_positive_comment():
-    import os
-
-    if os.path.exists(comment_test_file):
-        os.remove(comment_test_file)
-
-    add_positive_comments(comment_test_file, ["Julian", "Dick"])
-    assert check_file_contents("+Julian, Dick\n")
+def test_add_positive_comment(comment_file):
+    add_positive_comments(comment_file, ["Julian", "Dick"])
+    assert check_file_contents(comment_file, "+Julian, Dick\n")
