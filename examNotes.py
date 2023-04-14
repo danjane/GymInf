565fab13f93449cde8dba55c2d45379a81c6ad8d
danjane <jdtane@gmail.com>
2023-04-14 21:50:13 +0200

    Add conditional formatting for students who are not passing

--- examNotes.py
+++ examNotes.py
@@ -20,15 +20,16 @@ def create_empty_spreadsheet_for_student_notes(student_codes: List[str], questio
         'bg_color': '#C6EFCE',  # light green background
         'font_color': '#006100',  # dark green font
     })
-    round_format = [workbook.add_format({'num_format': '0.00'}),
-                    workbook.add_format({'num_format': '0.0', 'bold': True})]
+    round_formats = [workbook.add_format({'num_format': '0.00'}),
+                     workbook.add_format({'num_format': '0.0', 'bold': True})]
+    red = workbook.add_format({'bg_color': 'red'})
 
     _write_headers(worksheet, question_names, question_weights, question_marks,
                    light_green_format)
     _write_student_names_and_empty_cells(worksheet, student_codes, row_for_students, num_questions,
                                          bold, light_green_format)
-    _add_formulae_to_calculate_notes(worksheet, num_students, num_questions, row_for_students,
-                                     col_for_total, col_for_unrounded_note, col_for_note, bold, round_format)
+    _add_formulae_to_calculate_notes(worksheet, num_students, num_questions, row_for_students, col_for_total,
+                                     col_for_unrounded_note, col_for_note, bold, round_formats, red)
 
     workbook.close()
 
@@ -49,7 +50,8 @@ def _write_student_names_and_empty_cells(worksheet, student_codes, row_for_stude
 
 
 def _add_formulae_to_calculate_notes(worksheet, num_students, num_questions, row_for_students,
-                                     col_for_total, col_for_unrounded_note, col_for_note, bold, round_format) -> None:
+                                     col_for_total, col_for_unrounded_note, col_for_note,
+                                     bold, round_formats, red) -> None:
     worksheet.write(0, col_for_total, "Total", bold)
     worksheet.write(0, col_for_unrounded_note, "UnroundedNote")
     worksheet.write(0, col_for_note, "Note", bold)
@@ -60,16 +62,22 @@ def _add_formulae_to_calculate_notes(worksheet, num_students, num_questions, row
         weight_cell = xlsxwriter.utility.xl_rowcol_to_cell(1, i + 1)
         mark_cell = xlsxwriter.utility.xl_rowcol_to_cell(2, i + 1)
         formula = f'=IF({mark_cell}>0, {weight_cell}/{mark_cell}, 0)'
-        worksheet.write_formula(row_for_working, i + 1, formula, round_format[0])
+        worksheet.write_formula(row_for_working, i + 1, formula, round_formats[0])
     working_range = xlsxwriter.utility.xl_range_abs(row_for_working, 1, row_for_working, num_questions)
 
     for i in range(num_students):
-        note_range = xlsxwriter.utility.xl_range(i + row_for_students, 1, i + row_for_students, num_questions)
-        formula = f'=SUMPRODUCT({note_range}, {working_range})'
-        worksheet.write_formula(i + row_for_students, col_for_total, formula, round_format[0])
+        mark_range = xlsxwriter.utility.xl_range(i + row_for_students, 1, i + row_for_students, num_questions)
+        formula = f'=SUMPRODUCT({mark_range}, {working_range})'
+        worksheet.write_formula(i + row_for_students, col_for_total, formula, round_formats[0])
         total_cell = xlsxwriter.utility.xl_rowcol_to_cell(i + row_for_students, col_for_total)
         formula_unrounded = f'=POWER({total_cell},1.3)*4.8+1.3'
-        worksheet.write_formula(i + row_for_students, col_for_unrounded_note, formula_unrounded, round_format[0])
+        worksheet.write_formula(i + row_for_students, col_for_unrounded_note, formula_unrounded, round_formats[0])
         unrounded_cell = xlsxwriter.utility.xl_rowcol_to_cell(i + row_for_students, col_for_unrounded_note)
         formula_rounded = f'=MAX(1.5, MIN(6, ROUND({unrounded_cell}*2, 0)/2))'
-        worksheet.write_formula(i + row_for_students, col_for_note, formula_rounded, round_format[1])
+        worksheet.write_formula(i + row_for_students, col_for_note, formula_rounded, round_formats[1])
+
+    note_range = xlsxwriter.utility.xl_range(row_for_students, col_for_note,
+                                             row_for_students + num_students - 1, col_for_note)
+    worksheet.conditional_format(note_range, {'type': 'cell', 'criteria': '<', 'value': 4, 'format': red})
+
+
