d51fb454037010d166d20931aff9549de3bc685f
danjane <jdtane@gmail.com>
2023-04-15 11:50:16 +0200

    Failing test for merging exams

--- tests/examNotes_test.py
+++ tests/examNotes_test.py
@@ -3,6 +3,7 @@ import students
 import os
 import datetime
 
+
 def test_create_empty_spreadsheet_for_notes(tmp_path):
     student_dict = students.parse_course_list("../example_files/1ma1df01.txt")
 
@@ -36,4 +37,19 @@ def test_read_notes_from_filename():
     expected = pd.DataFrame(data)
     expected.set_index('Student', inplace=True)
     loaded = read_notes_from_filename(test_file)
+    assert expected.equals(loaded)
+
+
+def test_merge_notes_for_one_course():
+    exam_folder = "../example_files/1ma1df01/"
+    students = ["albert.enstn", "gabriel.crmr", "marie.cr", "richard.fynmn"]
+
+    data = {"Student": ["albert.enstn", "gabriel.crmr", "marie.cr", "richard.fynmn"],
+            "20Apr2020": [3, 2.5, 4, 6],
+            "20May2020": [3, 4.5, 3, 4]}
+    expected = pd.DataFrame(data)
+    expected.set_index('Student', inplace=True)
+
+    loaded, exam_names = merge_notes_for_one_course(exam_folder, students)
+    assert exam_names == ["ExampleExam", "SecondExam"]
     assert expected.equals(loaded)
