3de23657f4ab6d7cb1f9515081be11919e109687
danjane <jdtane@gmail.com>
2023-07-26 23:50:07 +0200

    Minor corrections

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,13 +1,12 @@
 import pygame
-from pygame.locals import *
-import constants
+import GUI.constants
 
 pygame.init()
 
 import class_view, control_view
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode(constants.WIDTH_HEIGHT)
+screen = pygame.display.set_mode(GUI.constants.WIDTH_HEIGHT)
 pygame.display.set_caption("TAT: teacher assisting tools")
 
 config_file = "../GUI_files/config.yaml"
@@ -19,9 +18,9 @@ while running:
     if next_view == "quit":
         running = False
     elif next_view == "control_view":
-        next_view, course = control_view.run(config_file, course, screen, clock, constants)
+        next_view, course = control_view.run(config_file, course, screen, clock, GUI.constants)
     elif next_view == "class_view":
-        next_view, course = class_view.run(config_file, course, screen, clock, constants)
+        next_view, course = class_view.run(config_file, course, screen, clock, GUI.constants)
     else:
         raise NotImplementedError("This view yet to be seen to!!")
 
