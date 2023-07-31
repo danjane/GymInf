e8153a92b88bcfbbd5be293548926871c5aa72b8
danjane <jdtane@gmail.com>
2023-07-31 16:15:15 +0200

    Running an executable from cxfreeze changes the working directory (to the app itself) so need to rework relative paths

--- config.py
+++ config.py
@@ -1,17 +1,30 @@
 import yaml
 import os
+import sys
 from typing import Dict, Any
 
 
+def adjust_path_for_cxfreeze(path):
+    working_dir, _ = os.path.split(sys.executable)
+    return os.path.join(working_dir, path)
+
+
+def cx(filename):
+    cxfreeze_executable_flag = getattr(sys, "frozen", False)
+    if cxfreeze_executable_flag:
+        return adjust_path_for_cxfreeze(filename)
+    else:
+        return filename
+
 def load(filename: str) -> Dict[str, Any]:
-    with open(filename, 'r') as f:
+    with open(cx(filename), 'r') as f:
         config = yaml.safe_load(f)
     config = add_class_paths(config)
     return config
 
 
 def add_class_paths(config: Dict[str, Any]) -> Dict[str, Any]:
-    folder = config["courses_path"]
+    folder = cx(config["courses_path"])
     courses = config["courses"]
     paths = [os.path.join(folder, cls + ".txt") for cls in courses]
     config["class_paths"] = paths
@@ -19,8 +32,8 @@ def add_class_paths(config: Dict[str, Any]) -> Dict[str, Any]:
 
 
 def update_courses_in_yaml(filename, courses):
-    with open(filename, 'r') as f:
+    with open(cx(filename), 'r') as f:
         config = yaml.safe_load(f)
     config["courses"] = courses
-    with open(filename, 'w') as f:
+    with open(cx(filename), 'w') as f:
         yaml.safe_dump(config, f)
